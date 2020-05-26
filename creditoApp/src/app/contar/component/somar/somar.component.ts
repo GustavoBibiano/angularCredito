b8bb4bda959de3bcb10d89aca93eabf63148f413
Gustavo Bibiano Lopes <gu.bibiano@hotmail.com>
2020-05-26 02:24:11 +0000

    Começando

---
+++ creditoApp/src/app/contar/component/somar/somar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SomarService } from '../../services';
+
+@Component({
+  selector: 'app-somar',
+  templateUrl: './somar.component.html',
+  styleUrls: ['./somar.component.css']
+})
+export class SomarComponent implements OnInit {
+  private valor1:number;
+  constructor(private somar: SomarService) { }
+
+  ngOnInit(): void {
+    this.valor1 = 0;
+  }
+  calcula(valorInserido: number){
+      this.valor1 = this.somar.somar(this.valor1, valorInserido);
+  }
+
+  get pegaValor(){
+    return this.valor1;
+  }
+}
---
+++ creditoApp/src/app/contar/contar.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SomarComponent } from './component/somar';
+import { SomarService } from './services';
+
+
+
+@NgModule({
+  declarations: [SomarComponent],
+  imports: [
+    CommonModule
+  ],exports:[
+    SomarComponent
+  ],
+providers:[
+  SomarService
+]
+})
+export class ContarModule { }
